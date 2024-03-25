a7216772b54497d40932a10b671b435b31c3b31a
ZacheryCalahan <zacherycalahan33@gmail.com>
2024-03-25 17:01:54 -0400

    shooter auto commit

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -6,20 +6,25 @@ public class Constants {
         public static final int kShooterPrimaryId = 16;
         public static final int kShooterSecondaryId = 17;
         public static final int kIntakeId = 15;
+        public static final int kElevatorPrimaryId = 18;
+        public static final int kElevatorSecondaryId = 19;
         
         // Mechanism Configs
-        public static final boolean kShooterInverted = false;
-        public static final boolean kIntakeInverted = false;
+        public static final boolean kShooterInverted = true;
+        public static final boolean kIntakeInverted = true;
 
         // Controller bindings
         //public static final int kShooterInButton = 1;
         public static final int kShooterOutButton = 5;
-        public static final int kIntakeInButton = 4;
-        public static final int kIntakeOutButton = 6;
+        public static final int kElevatorUpButton = 4;
+        public static final int kElevatorDownButton = 1;
+        public static int kIntakeOutButton;
+        public static int kIntakeInButton;
     }
 
     public static class AutoConstants {
         public static final double kTimeForward = 2.2; // Time forward in seconds
         public static final double kSpeedForward = .3; // Speed forward (no need to change).
+        public static final double kshootautotimer = 1.3;
     }
 }
